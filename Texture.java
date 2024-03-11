756c6223d918315c44ec273b300d14d4cda74ead
cocamc <131023050+cocamc@users.noreply.github.com>
2024-03-11 23:43:42 +0000

    Create Texture.java

---
+++ Texture.java
@@ -0,0 +1,26 @@
+
+import net.minecraft.util.Identifier;
+
+public class Texture extends Identifier {
+    public Texture(String path) {
+        super("YOUR_ID", validatePath(path));
+    }
+
+    public Texture(Identifier i) {
+        super(i.getNamespace(), i.getPath());
+    }
+
+    static String validatePath(String path) {
+        if (isValid(path)) {
+            return path;
+        }
+        StringBuilder ret = new StringBuilder();
+        for (char c : path.toLowerCase().toCharArray()) {
+            if (isPathCharacterValid(c)) {
+                ret.append(c);
+            }
+        }
+        return ret.toString();
+    }
+
+}
